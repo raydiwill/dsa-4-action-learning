773802ca639f8154a5250a89c3c87843624a821a
raydiwill <duong.tranhn1102@gmail.com>
2024-01-29 10:57:55 +0100

    Updated base working api

--- api/main.py
+++ api/main.py
@@ -1,8 +1,32 @@
-from fastpi import FastAPI, Depends
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import and_
+from models import Base, CustomerModel, ProblemStats, CustomerReport
+from setup_db import *
+from schema import CustomerData
+from datetime import datetime
 import pandas as pd
 import uvicorn
 
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    application = FastAPI(title=settings.PROJECT_NAME,
+                          version=settings.PROJECT_VERSION)
+    create_tables()
+    return application
+
+
+app = start_application()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8050)
