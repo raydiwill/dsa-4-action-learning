278efcc5b7ff3d2c1dbbe13865422df130dc59b9
raydiwill <duong.tranhn1102@gmail.com>
2024-01-29 11:15:47 +0100

    Updated input data for api

--- api/schema.py
+++ api/schema.py
@@ -2,4 +2,18 @@ from pydantic import BaseModel
 
 
 class CustomerData(BaseModel):
-    pass
+    id: str
+    region: str
+    tenure: str
+    amount: float
+    frequence_rech: float
+    revenue: float
+    arpu_segment: float
+    frequence: float
+    data_volume: float
+    on_net: float
+    orange: float
+    tigo: float
+    regularity: int
+    top_pack: str
+    freq_top_pack: float
