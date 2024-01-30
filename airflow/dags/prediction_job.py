ddc41158140f7f56dcfa9321717c26996a0691ae
raydiwill <duong.tranhn1102@gmail.com>
2024-01-30 12:40:40 +0100

    Fixed multiple prediction job

--- airflow/dags/prediction_job.py
+++ airflow/dags/prediction_job.py
@@ -1,27 +1,44 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
+from email.mime.text import MIMEText
 import pandas as pd
 import logging
 import requests
 import os
+import smtplib
 
-API_URL = "http://host.docker.internal:8050/predict/"
+POST_URL = "http://host.docker.internal:8050/predict/"
+GET_URL = "http://host.docker.internal:8050/past-predictions/"
 folder_path = "/opt/data/good"
+user_email = "duong.tranhn1102@gmail.com"
+
+
+def send_email(sender, recipient, subject, message):
+    # Create the message
+    message = MIMEText(message)
+    message["Subject"] = subject
+    message["From"] = sender
+    message["To"] = recipient
+
+    # Establish a connection with the SMTP server
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(user_email, "ulws pdlo avlh oggs")
+        server.sendmail(sender, recipient, message.as_string())
 
 
 @dag(
     dag_id='prediction_job',
     description='Take files and output predictions',
     tags=['dsp', 'prediction_job'],
-    schedule=timedelta(minutes=1),
+    schedule=timedelta(minutes=2),
     start_date=days_ago(n=0, hour=1),
     catchup=False
 )
 def prediction_job():
     @task
     def check_for_new_data(path):
-        
         csv_files = [file for file in os.listdir(path) if
                      file.endswith(".csv") and
                      not file.startswith("predicted_")]
@@ -42,9 +59,9 @@ def prediction_job():
 
     @task
     def make_predictions(df):
-        prediction_data = {}
+        prediction_data = []
         for _, row in df.iterrows():
-            prediction_data = {
+            row_data = {
                 "user_id": row["user_id"],
                 "REGION": row["REGION"] if not pd.isna(
                     row["REGION"]) else None,
@@ -74,16 +91,60 @@ def prediction_job():
                 "FREQ_TOP_PACK": row["FREQ_TOP_PACK"] if not pd.isna(
                     row["FREQ_TOP_PACK"]) else None,
             }
+            prediction_data.append(row_data)
+
         response = requests.post(
-            API_URL,
+            POST_URL,
             json=prediction_data
         )
 
         response_data = response.json()
         logging.info(f'{response_data}')
+        return response.status_code
 
+    @task
+    def get_past_predictions(status):
+        dates = {
+            "start_date": (datetime.today()).strftime(
+                "%Y-%m-%d"),
+            "end_date": (datetime.now() + timedelta(days=1)).strftime(
+                "%Y-%m-%d")
+        }
+
+        response = requests.get(
+            GET_URL,
+            json=dates
+        )
+
+        past_predictions = pd.DataFrame(response.json())
+        return past_predictions.to_dict('records')
+
+    @task
+    def notify_team(predictions):
+        sender = user_email
+        recipient = "duong-khanh.tran@epita.fr"
+
+        high_risk = [pred for pred in predictions if
+                     pred['pred_risk'] == 'High']
+        if high_risk:
+            subject = "High-Risk Churn predictions detected"
+            body = (f'Dear Analysis team,\n\n'
+                    f'There are {len(high_risk)} high-risk churners detected.'
+                    f'\n\nPlease check dashboard.')
+            send_email(sender, recipient, subject, body)
+            logging.info(f'Email sent!')
+        else:
+            subject = "Churn predictions notification"
+            body = (f'Dear Analysis team,\n'
+                    f'\nThere are churners detected by the model.\n')
+            send_email(sender, recipient, subject, body)
+            logging.info(f'Email sent!')
+
+    # Task
     df_to_predict = check_for_new_data(folder_path)
-    make_predictions(df_to_predict)
+    status_code = make_predictions(df_to_predict)
+    pred_dict = get_past_predictions(status_code)
+    #notify_team(pred_dict)
 
 
 scheduled_job_dag = prediction_job()
