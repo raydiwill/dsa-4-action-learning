85c78c01c66986c8abba7eb4298129f8cbb48016
raydiwill <duong.tranhn1102@gmail.com>
2024-02-15 21:38:29 +0100

    Added recommendations page

--- ui/main.py
+++ ui/main.py
@@ -1,34 +1,9 @@
-from chatbot import chatbot_ui
 from interactive_dashboard import interactive_dashboard_page
 from past_predictions import past_predictions_page
+from send_recommendations import send_recommendations_page
+from chatbot import chatbot_ui
 from utils import *
 import streamlit as st
-import pandas as pd
-
-
-def send_recommendations_page():
-    st.title("Send Recommendation to Customer Service")
-
-    # Check if the user is logged in
-    if not is_user_logged_in():
-        st.warning("Please log in to access this page.")
-        return
-
-    # Logout button
-    if st.button("Logout"):
-        # Clear session state to log out the user
-        del st.session_state.is_logged_in
-        st.success("Logout successful!")
-        st.experimental_rerun()
-
-    to_address = st.text_input("Recipient Email")
-    subject = st.text_input("Subject")
-    message = st.text_area("Message")
-
-    if st.button("Send Email"):
-        if send_email(user_email, to_address, subject, message):
-            print("Sent!")
-        st.toast("Email sent successfully.", icon="🎉")
 
 
 def main():
